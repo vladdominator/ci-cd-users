f8320e6bff26a730804aa8d89c9aca1ecd6011a9
Vladislav Zhilinskiy <vlad.zhilinskiy2003@inbox.ru>
2023-12-30 00:52:47 +0300

    fix: dkdk

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
   app.useGlobalPipes(new ValidationPipe({}));
+
   await app.listen(3000);
 }
 
